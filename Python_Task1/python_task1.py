35e3536720f788c30a4ab80747d5b5c060e4920d
JARVIS <muditgupta68@gmail.com>
2020-06-10 19:55:59 +0530

    .py file successful

---
+++ Python_Task1/python_task1.py
@@ -0,0 +1,33 @@
+print("------------------------------------------------------------------------------------")
+print("Python course by team CODEWAYY!")
+print("String Concatenation!")
+
+print("\n") #new line
+
+print("My name is ")
+first_name="Mudit " #My First Name   
+last_name="Gupta"   #My last Name
+Fullname=first_name + last_name    #String Concatenation
+print(Fullname) #printing full name using string concatenation
+print("\n")  
+print("My college name is:-")  
+cname="Amity "        #college first name
+lname="University "   #college last name
+address="Uttarpradesh" #address of college
+collegename=cname+lname+address #string concatenation
+print(collegename) #printing college details
+ 
+print("\n")  #new line
+
+print("---------------------------------------Task 1 Begins-------------------------------------------------")
+maths=70  
+english=80
+science=90
+comp=99
+sst=60    #subject got initialised
+total=maths+english+science+comp+sst  #logic to add the total marks
+print("subjects are: \n\n Mathematics \n English \n Science \n Computer science \n Social science.") #Name of the subjects
+print("\nTotal marks is: ",total) #displaying marks
+perc= total/5   # percentage logic
+print("The percentage obtained is: ",perc) # displaying percentage obtained!
+print("\n------------------------------------Task1 Completed----------------------------------------")

29f5acb802aa75cac481453c837791cb941913e2
Mudit Gupta <54271833+muditgupta68@users.noreply.github.com>
2020-06-24 14:29:51 +0530

    float-int file commited

---
+++ Python_Task1/task1.py
@@ -0,0 +1,24 @@
+# initialising the values from the user
+int1 = int(input("enter the integer number"))
+int2 = int(input("enter the integer number"))
+flt1 = float(input("enter the float value"))
+flt2 = float(input("enter the float value"))
+print("\n\n")
+# ----------------------------------------------------------------------------------------------------------------------
+# working with the integer values
+print("sum of the integers:  ",int1+int2)
+print("difference of the integers:  ",int1-int2)
+print("multiplication of the integers:  ",int1*int2)
+print("division of the integers:  ",int1/int2)
+print("remainder of the integers:  ",int1%int2)
+print("power of the integers:  ",int1**int2)
+print("\n\n")
+# ----------------------------------------------------------------------------------------------------------------------
+# working with float values
+print("addition of the float values:  ",flt1+flt2)
+print("difference of the float values:  ",flt1-flt2)
+print("product of the float values:  ",flt1*flt2)
+print("division of the float values:  ",flt1/flt2)
+print("remainder of the float values:  ",flt1%flt2)
+print("power of the float values:  ",flt1**flt2)
+# ----------------------------------------------------------------------------------------------------------------------

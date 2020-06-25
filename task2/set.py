43e3a1e7d7b28ed3378cde1d6af3a5e61ae67baf
Mudit Gupta <54271833+muditgupta68@users.noreply.github.com>
2020-06-25 12:25:29 +0530

    Update set.py

--- task2/set.py
+++ task2/set.py
@@ -1,24 +1,30 @@
-c_lang = {'C++','java','django','python','CS'}  #set initiated
+c_lang = {'C++','java','django','python','CS'}  
+#set initiated
 print(c_lang)
 
 # add function
-c_lang.add("HTML")  # add() will add the desired data
+c_lang.add("HTML")  
+# add() will add the desired data
 print(c_lang)
 
 # remove function
-c_lang.remove("HTML")  # remove() deletes the data from the set
+c_lang.remove("HTML")  
+# remove() deletes the data from the set
 print(c_lang)
 
 # copy function
-language=c_lang.copy()  # copy() creates a copy of set
+language=c_lang.copy()  
+# copy() creates a copy of set
 print(language)
 
 # pop function
-c_lang.pop()  # pop() deletes the data randomly
+c_lang.pop()  
+# pop() deletes the data randomly
 print(c_lang)
 
 # length function
-length = len(language)  # len() tells the length of the set
+length = len(language)  
+# len() tells the length of the set
 print(length)
 
 # clearing the set

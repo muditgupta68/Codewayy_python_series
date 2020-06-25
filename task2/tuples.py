c2c2a0768db0cebc423f2076c9e4950ba4826120
Mudit Gupta <54271833+muditgupta68@users.noreply.github.com>
2020-06-25 12:21:31 +0530

    Update tuples.py

--- task2/tuples.py
+++ task2/tuples.py
@@ -1,10 +1,12 @@
-name = ('mudit','deepak','riya','rahul','mudit')  # tuple initiated
+name = ('mudit','deepak','riya','rahul','mudit')  
+# tuple initiated
 print(name)
 
 # tuples are immutable whereas lists are mutable
 
 # counting function
-total = name.count('mudit')  # count() will count the data in the tuple
+total = name.count('mudit')  
+# count() will count the data in the tuple
 print("total count of mudit in the tuple :",total)
 
 # converting tuple into list
@@ -13,16 +15,19 @@ print(lname)
 
 # length function
 length = len(name)
-print(length)  # len() will print the length of the tuple
+# len() will print the length of the tuple
+print(length)  
 
 # index function
-x = name.index('riya')  # index() will tell the index of the data
+x = name.index('riya')  
+# index() will tell the index of the data
 print(x)
 
 lst = [1,2,3,4,5,6]  # list initiated
 print(lst)
 # converting list into tuple
-tname = tuple(lst)  # list is converted to tuple
+tname = tuple(lst)  
+# list is converted to tuple
 print(tname)
 
 
